8cd8231e0f60f4d70bfa1e6367ef1d7022536dea
barbie.bleue <barbie.bleue@8120040e-99f6-f31c-11d1-4badcc726a8f>
2010-10-20 14:46:39 +0000

    Agente Proveedor

---
+++ trunk/restaurant-agent/src/comportamientos/ContratarBodego.java
@@ -0,0 +1,31 @@
+package comportamientos;
+
+import util.Mensaje;
+import util.Performativas;
+import agentes.AgenteProveedor;
+import jade.core.AID;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.states.MsgReceiver;
+
+public class ContratarBodego extends MsgReceiver{
+
+	private static final long serialVersionUID = -4902283519265681261L;
+	AgenteProveedor miAgente;
+	
+	public ContratarBodego(AgenteProveedor a, long deadline){
+		//Agent, MessageTemplate, deadline, DataStore, msgKey
+		super(a,MessageTemplate.MatchPerformative(Performativas.CONFIRMAR),deadline,null,null);
+		miAgente=a;
+	}
+	
+	protected void handleMessage(ACLMessage msg) {
+		if(msg==null){
+			Mensaje.mandaMensaje(miAgente, Performativas.INFORMAR, miAgente.agentesBodegos, miAgente.ingredientePorAcomodar.clave);
+			myAgent.addBehaviour(new ContratarBodego(miAgente,2000L));
+		}else{
+			Mensaje.mandaMensaje(miAgente, Performativas.CONFIRMAR, new AID[]{msg.getSender()}, ""); 
+			miAgente.ingredientePorAcomodar=null;
+		}
+	}
+}
---
+++ trunk/restaurant-agent/src/comportamientos/RevisarPeticiones.java
@@ -0,0 +1,34 @@
+package comportamientos;
+
+import agentes.AgenteProveedor;
+import sql.Ingrediente;
+import sql.Peticion;
+import util.Mensaje;
+import util.Performativas;
+import jade.core.behaviours.CyclicBehaviour;
+
+
+public class RevisarPeticiones extends CyclicBehaviour {
+
+	AgenteProveedor miAgente;
+	
+	public RevisarPeticiones(AgenteProveedor a) {
+		super(a);
+		miAgente = a;
+	}
+
+	private static final long serialVersionUID = -8423084959540874770L;
+
+	@Override
+	public void action() {
+		if(miAgente.ingredientePorAcomodar==null){
+			Ingrediente ingredientePedido = Peticion.obtenerPeticion();
+			if(ingredientePedido!=null){
+				miAgente.ingredientePorAcomodar = ingredientePedido;
+				Mensaje.mandaMensaje(miAgente, Performativas.INFORMAR, miAgente.agentesBodegos, ingredientePedido.clave);
+				myAgent.addBehaviour(new ContratarBodego(miAgente,2000L));
+			}
+		}
+	}
+
+}
