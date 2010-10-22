420fb355d2e0fc85b920a5f27f421c2a0ffbd34e
golden.darknut <golden.darknut@8120040e-99f6-f31c-11d1-4badcc726a8f>
2010-10-22 23:15:06 +0000

    Añadidos scripts para Recetas e ingredientes

--- trunk/scripts/ingredientes.sql
+++ trunk/scripts/ingredientes.sql
@@ -1,5 +1,77 @@
 insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
-   values ('leche', 'Leche', 1000, 10, 0, 30, 12);
+   values ('lechu', 'Lechuga', 0, 0, 0, 0, 3);
 
 insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
-   values ('pan', 'Pan', 200, 3, 0, 10, 20);
+   values ('jitom', 'Jitomate', 0, 0, 0, 0, 15);
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('huevo', 'Huevo', 0, 0, 0, 0, 12);
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('camrn', 'Camarones', 0, 0, 0, 0, 20);
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('arroz', 'Arroz', 0, 0, 0, 0, 1);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('salmn', 'Salmon', 0, 0, 0, 0, 3);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('lngst', 'Langosta', 0, 0, 0, 0, 1);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('mntql', 'Lechuga', 0, 0, 0, 0, 3);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('croja', 'Carne roja', 0, 0, 0, 0, 4);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('papa', 'Papa', 0, 0, 0, 0, 20);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('pasta', 'Pasta', 0, 0, 0, 0, 3);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('queso', 'Queso', 0, 0, 0, 0, 5);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('chile', 'Chile', 0, 0, 0, 0, 30);
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('trtll', 'Tortilla', 0, 0, 0, 0, 15);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('limon', 'Limon', 0, 0, 0, 0, 20);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('pollo', 'Pollo', 0, 0, 0, 0, 1);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('frjls', 'Frijoles', 0, 0, 0, 0, 1);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('almjs', 'Almejas', 0, 0, 0, 0, 10);   
+
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('chmpñ', 'Champiñones', 0, 0, 0, 0, 25);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('tofu', 'Tofu', 0, 0, 0, 0, 1);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('tocno', 'Tocino', 0, 0, 0, 0, 10);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('slchi', 'Salchica', 0, 0, 0, 0, 8);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('ajo', 'Ajo', 0, 0, 0, 0, 12);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('chich', 'Chicharos', 0, 0, 0, 0, 50);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('pmnto', 'Pimiento', 0, 0, 0, 0, 1);   
+   
+insert into ingrediente (clave, nombre, peso, costo, refrigerado, caducidad, cantidadPorPaquete)
+   values ('cblla', 'Cebolla', 0, 0, 0, 0, 8);   
--- trunk/scripts/recetas.sql
+++ trunk/scripts/recetas.sql
@@ -1,2 +1,57 @@
-insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2)
- values('dsyno', 'desayuno', 'leche', 1, 'pan', 1);
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('salad', 'Ensalada de huevo', 'lechu', 1, 'huevo', 2, 'jitom', 3);
+	
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('sushi', 'Sushi', 'camrn', 5, 'arroz', 1, 'salmn', 1);	
+
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('lngst', 'Langosta a la matequilla', 'lngst', 1, 'mntql', 2, 'lechu', 1);	
+	
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('tbone', 'Corte T-bone', 'croja', 1, 'papa', 5, 'lechu', 1);
+	
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('spagt', 'Spaghetti a la Bolognesa', 'pasta', 1, 'croja', 1, 'jitom', 3);	
+	
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('lasgn', 'Lasagna', 'croja', 1, 'queso', 2, 'pasta', 1);	
+	
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('nacho', 'Nachos', 'chile', 3, 'trtll', 2, 'queso', 2);	
+	
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('splng', 'Sopa de langosta', 'lechu', 1, 'huevo', 2, 'jitom', 3);	
+
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('pollo', 'Pollo rostizado', 'pollo', 1, 'papa', 5, 'lechu', 1);	
+
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3, ingrediente4, cantidad4)
+    values('enchi', 'Enchiladas', 'trtll', 5, 'pollo', 1, 'chile', 2, 'crema', 1);	
+
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3, ingrediente4, cantidad4)
+    values('tstds', 'Tostadas', 'trtll', 5, 'pollo', 1, 'frjls', 1, 'jitom', 3);	
+
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('paela', 'Paella', 'camrn', 7, 'arroz', 1, 'almjs', 3);
+
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('almjs', 'Almejas a la italiana', 'almjs', 7, 'chmpñ', 5, 'queso', 2);
+
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('spvgt', 'Sopa vegetariana', 'pasta', 2, 'chmpñ', 5, 'tofu', 1);
+
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3, ingrediente4, cantidad4)
+    values('ingls', 'Desayuno ingles', 'huevo', 3, 'tocno', 5, 'slchi', 3, 'chmpñ', 5);	
+
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('aljit', 'Almejas con jitomate', 'almjs', 5, 'jitom', 4, 'ajo', 3);
+
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('chmpñ', 'Crema de champiñones', 'chmpñ', 8, 'tocno', 3, 'chich', 20);
+
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3, ingrediente4, cantidad4)
+    values('arroz', 'Arroz cremoso con salmon', 'salmn', 1, 'arroz', 1, 'ajo', 4, 'pmnto', 1);	
+
+insert into receta(clave, nombre, ingrediente1, cantidad1, ingrediente2, cantidad2, ingrediente3, cantidad3)
+    values('cevic', 'Ceviche de salmon', 'salmn', 1, 'pmnto', 1, 'cblla', 3);
+
